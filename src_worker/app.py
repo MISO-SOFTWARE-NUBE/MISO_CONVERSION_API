ddf1734f677cda14c6a3c061eac75dcb96276034
Andres Martinez <af.martinezc1@uniandes.udu.co>
2023-11-25 15:13:29 -0500

    Conversion correcta del byte

--- src_worker/app.py
+++ src_worker/app.py
@@ -122,7 +122,7 @@ class ProcesarTarea(Resource):
         bodyMessage = request.json.get("message").get("data")
         bodyText = base64.b64decode(bodyMessage)
         print(bodyText)
-        id = str(bodyText)
+        id = bodyText.decode("utf-8")
         try:
             process_task(id)
             return {"mensaje": "Tarea procesada correctamente"}, 200
