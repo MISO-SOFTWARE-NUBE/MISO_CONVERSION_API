bffada2d6d9029accb4e428c633bb712e649af26
snader21 <s.nader21@uniandes.edu.co>
2023-11-28 21:17:09 -0500

    Se realizan cambios en el multithreading

--- src_worker/app.py
+++ src_worker/app.py
@@ -55,65 +55,65 @@ client = storage.Client()
 bucket = client.bucket(current_app.config['UPLOAD_BUCKET'])
 
 
-def process_task(id):
-    try:
-        print("Id:", id)
-        # 1. Query record in database
-        record = db.session.query(Solicitudes).get(int(id))
+def process_task(app, id):
+    with app.app_context():
+        try:
+            # 1. Query record in database
+            record = db.session.query(Solicitudes).get(int(id))
 
-        # 2. Register start_processing_time and update status
-        record.start_process_date = datetime.now()
-        record.status = "in_process"
-        db.session.commit()
-
-        # 3. Get blob names from db
-        input_blob_name = get_blob_name_from_gs_uri(record.input_path)
-        output_blob_name = get_blob_name_from_gs_uri(record.output_path)
-
-        # Get gcp blobs for input and output
-        input_blob = bucket.blob(input_blob_name)
-        output_blob = bucket.blob(output_blob_name)
-
-        # Download the input file to a temp file
-        with tempfile.NamedTemporaryFile() as temp_input_file:
-            input_blob.download_to_filename(temp_input_file.name)
-            fd_out, temp_output_file_name = tempfile.mkstemp(
-                suffix=f'.{record.output_format}')
-            os.close(fd_out)
-
-            cmd = [
-                'ffmpeg',
-                '-y',
-                '-f', record.input_format,
-                '-i', temp_input_file.name,
-                temp_output_file_name
-            ]
-
-            result = subprocess.run(cmd, capture_output=True, text=True)
-
-            if result.returncode != 0:
-                print("Error converting file:",
-                      result.stderr, result.stdout)
-                record.status = "failed"
-            else:
-                if os.path.getsize(temp_output_file_name) > 0:
-                    with open(temp_output_file_name, 'rb') as temp_output_file:
-                        output_blob.upload_from_file(
-                            temp_output_file, content_type=f'video/{record.output_format}')
-                    record.status = "available"
-                else:
-                    print("Conversion resulted in an empty file.")
-                    record.status = "failed"
-
-            record.end_process_date = datetime.now()
+            # 2. Register start_processing_time and update status
+            record.start_process_date = datetime.now()
+            record.status = "in_process"
             db.session.commit()
-    except Exception as e:
-        db.session.rollback()
-        record.status = "failed"
-        raise e
-    finally:
-        if os.path.exists(temp_output_file_name):
-            os.remove(temp_output_file_name)
+
+            # 3. Get blob names from db
+            input_blob_name = get_blob_name_from_gs_uri(record.input_path)
+            output_blob_name = get_blob_name_from_gs_uri(record.output_path)
+
+            # Get gcp blobs for input and output
+            input_blob = bucket.blob(input_blob_name)
+            output_blob = bucket.blob(output_blob_name)
+
+            # Download the input file to a temp file
+            with tempfile.NamedTemporaryFile() as temp_input_file:
+                input_blob.download_to_filename(temp_input_file.name)
+                fd_out, temp_output_file_name = tempfile.mkstemp(
+                    suffix=f'.{record.output_format}')
+                os.close(fd_out)
+
+                cmd = [
+                    'ffmpeg',
+                    '-y',
+                    '-f', record.input_format,
+                    '-i', temp_input_file.name,
+                    temp_output_file_name
+                ]
+
+                result = subprocess.run(cmd, capture_output=True, text=True)
+
+                if result.returncode != 0:
+                    print("Error converting file:",
+                          result.stderr, result.stdout)
+                    record.status = "failed"
+                else:
+                    if os.path.getsize(temp_output_file_name) > 0:
+                        with open(temp_output_file_name, 'rb') as temp_output_file:
+                            output_blob.upload_from_file(
+                                temp_output_file, content_type=f'video/{record.output_format}')
+                        record.status = "available"
+                    else:
+                        print("Conversion resulted in an empty file.")
+                        record.status = "failed"
+
+                record.end_process_date = datetime.now()
+                db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            record.status = "failed"
+            raise e
+        finally:
+            if os.path.exists(temp_output_file_name):
+                os.remove(temp_output_file_name)
 
 
 class ProcesarTarea(Resource):
@@ -121,7 +121,7 @@ class ProcesarTarea(Resource):
         bodyMessage = request.json.get("message").get("data")
         bodyText = base64.b64decode(bodyMessage)
         id = bodyText.decode("utf-8")
-        thread = threading.Thread(target=process_task, args=(id,))
+        thread = threading.Thread(target=process_task, args=(app, id,))
         thread.start()
         return {"mensaje": "Tarea procesada correctamente"}, 200
 
